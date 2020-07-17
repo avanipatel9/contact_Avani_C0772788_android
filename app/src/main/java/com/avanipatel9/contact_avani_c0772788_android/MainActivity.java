0b4403ee6f86a802dc8ada848e9c3486db40eef1
Avani Patel <anpatel36@yahoo.in>
2020-07-16 23:21:49 -0400

    Initial commit

---
+++ app/src/main/java/com/avanipatel9/contact_avani_c0772788_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avanipatel9.contact_avani_c0772788_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
